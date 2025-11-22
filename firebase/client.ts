97527158144820346bf005374c299cfbf87c3cb3
22f3000965 <22f3000965@ds.study.iitm.ac.in>
2025-11-22 09:46:22 +0530

    fix firebase config

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,14 +8,15 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDNwvQ6YLivTZpv6hk7PY0Tw0GCBBrNAaU",
-    authDomain: "aceprep-e268d.firebaseapp.com",
-    projectId: "aceprep-e268d",
-    storageBucket: "aceprep-e268d.firebasestorage.app",
-    messagingSenderId: "729277605893",
-    appId: "1:729277605893:web:ebcf71b47eb535fb85d46c",
-    measurementId: "G-CDC96PHETH"
+  apiKey: "AIzaSyDNwvQ6YLivTZpv6hk7PY0Tw0GCBBrNAaU",
+  authDomain: "aceprep-e268d.firebaseapp.com",
+  projectId: "aceprep-e268d",
+  storageBucket: "aceprep-e268d.firebasestorage.app",
+  messagingSenderId: "729277605893",
+  appId: "1:729277605893:web:ebcf71b47eb535fb85d46c",
+  measurementId: "G-CDC96PHETH"
 };
 
 // Initialize Firebase
